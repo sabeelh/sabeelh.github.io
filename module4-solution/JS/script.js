3a5b193f346ece26bdfee316f3ab511130533f75
sabeelh <68989784+sabeelh@users.noreply.github.com>
2020-09-04 15:18:50 +0500

    updating files
    updating files (for listing button)

--- module4-solution/JS/script.js
+++ module4-solution/JS/script.js
@@ -16,3 +16,20 @@ function show (){
 
 })(window);
 };
+
+function toconsole() {
+	(function (window) {
+		var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+		for (var i = 0; i < names.length; i++) {
+
+			var firstLetter = names[i].charAt(0).toLowerCase();
+
+			if (firstLetter == 'j') {
+    			window.bye.speak(names[i]);
+  			} else {
+    			window.hello.speak(names[i]);
+  			}
+  		}
+  	})(window);
+  };
